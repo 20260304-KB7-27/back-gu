675766d98fcc0e9cd51f2eca325aa44314f797a2
nhkim06 <nhkim7788@gmail.com>
2026-04-03 16:56:36 +0900

    feat : add knh page

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: () => import('@/pages/Home.vue'),
-        },
-        {
-            path: '/kch',
-            name: 'kch',
-            component: () => import('@/pages/kch_page.vue'),
-        },
-        {
-            path: '/kth',
-            name: 'kth',
-            component: () => import('@/pages/kth_page.vue'),
-        },
-    ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/pages/Home.vue'),
+    },
+    {
+      path: '/kch',
+      name: 'kch',
+      component: () => import('@/pages/kch_page.vue'),
+    },
+    {
+      path: '/kth',
+      name: 'kth',
+      component: () => import('@/pages/kth_page.vue'),
+    },
+    {
+      path: '/knh',
+      name: 'knh',
+      component: () => import('@/pages/knh_page.vue'),
+    },
+  ],
 });
 
 export default router;
