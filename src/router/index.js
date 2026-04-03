1e67d7da55fd782b2ac2d9da59bef1a094b1bd03
Nahyun <65719936+nhkim06@users.noreply.github.com>
2026-04-03 17:07:02 +0900

    [ #5 ] add knh page (#12)
    * feat : add knh page
    
    * #5 feat : add knh page

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,28 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: () => import('@/pages/Home.vue') },
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/pages/Home.vue'),
+    },
     {
       path: '/kch',
       name: 'kch',
       component: () => import('@/pages/kch_page.vue'),
     },
     {
-      path: '/1',
+      path: '/kth',
+      name: 'kth',
+      component: () => import('@/pages/kth_page.vue'),
+    },
+    {
+      path: '/knh',
+      name: 'knh',
+      component: () => import('@/pages/knh_page.vue'),
+    },
+    {
+      path: '/kjy',
       name: 'kjy',
       component: () => import('@/pages/KimJuyoung.vue'),
     },
