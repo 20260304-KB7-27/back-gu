740a8f2adf3fe81edb3b683e69e48de368d44c71
1juyoung <myjuyoung2@gmail.com>
2026-04-03 16:47:49 +0900

    feat: juyoung page upload #3

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: () => import('@/pages/Home.vue')},
-    { path: '/kch', name: 'kch', component: () => import('@/pages/kch_page.vue')}
+    { path: '/', name: 'home', component: () => import('@/pages/Home.vue') },
+    {
+      path: '/kch',
+      name: 'kch',
+      component: () => import('@/pages/kch_page.vue'),
+    },
+    {
+      path: '/1',
+      name: 'kjy',
+      component: () => import('@/pages/KimJuyoung.vue'),
+    },
   ],
-})
+});
 
 export default router;
