a235083b2f330fdb1f008253db5719bd9668eb2f
Charles <rhcharles0@gmail.com>
2026-04-03 16:13:24 +0900

    feat: add kth page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    { path: '/', name: 'home', component: () => import('@/pages/Home.vue')},
-    { path: '/kch', name: 'kch', component: () => import('@/pages/kch_page.vue')}
-  ],
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('@/pages/Home.vue'),
+        },
+        {
+            path: '/kch',
+            name: 'kch',
+            component: () => import('@/pages/kch_page.vue'),
+        },
+        {
+            path: '/kth',
+            name: 'kth',
+            component: () => import('@/pages/kth_page.vue'),
+        },
+    ],
+});
 
 export default router;
